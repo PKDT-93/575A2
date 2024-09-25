7941c8fb507d52e0b025464b996fc8999374f3c2
Peter Tran <peterkhoatran@gmail.com>
2024-09-25 13:07:59 -0600

    feat: wrapped mainview in a scroll view to stop content from being pushed up from a keyboard view

--- CPSC575A2/Views/ContentView.swift
+++ CPSC575A2/Views/ContentView.swift
@@ -19,43 +19,44 @@ struct ContentView: View {
     
     var body: some View {
         ZStack{
-            VStack{
-                IconView()
-                    .padding(.bottom, 50)
-                InputFieldView(
-                    label: "Name",
-                    placeholder: "Type in a name",
-                    text: $inputName,
-                    keyboardType: .default,
-                    action: {
-                        viewModel.setName(inputName)
-                    }
-                )
-                // Number Input Field
-                InputFieldView(
-                    label: "Number",
-                    placeholder: "Type in a number",
-                    text: $inputNumber,
-                    keyboardType: .numberPad,
-                    actionTitle: "Set",
-                    sysImg: "number",
-                    action: {
-                        viewModel.setNumber(inputNumber)
-                    }
-                )
-                Divider()
-                    .frame(width: 300)
-                    .padding(10)
-                
-                Text("\(viewModel.getName())")
-                    .padding(10)
-                Text("\(viewModel.getNumber())")
-                    .padding(.bottom, 25)
-                
-                SliderView(value: $sliderNumber)
-                    .padding(.bottom, 25)
-                TabContentView(isSwitchOn: $isSwitchOn, alert: $alert)
-                
+            ScrollView{
+                VStack{
+                    IconView()
+                        .padding(.bottom, 50)
+                    InputFieldView(
+                        label: "Name",
+                        placeholder: "Type in a name",
+                        text: $inputName,
+                        keyboardType: .default,
+                        action: {
+                            viewModel.setName(inputName)
+                        }
+                    )
+                    // Number Input Field
+                    InputFieldView(
+                        label: "Number",
+                        placeholder: "Type in a number",
+                        text: $inputNumber,
+                        keyboardType: .numberPad,
+                        actionTitle: "Set",
+                        sysImg: "number",
+                        action: {
+                            viewModel.setNumber(inputNumber)
+                        }
+                    )
+                    Divider()
+                        .frame(width: 300)
+                        .padding(10)
+                    
+                    Text("\(viewModel.getName())")
+                        .padding(10)
+                    Text("\(viewModel.getNumber())")
+                        .padding(.bottom, 25)
+                    
+                    SliderView(value: $sliderNumber)
+                        .padding(.bottom, 25)
+                    TabContentView(isSwitchOn: $isSwitchOn, alert: $alert)
+                }
             }
         }
         .padding()
